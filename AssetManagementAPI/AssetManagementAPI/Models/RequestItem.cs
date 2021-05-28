ce0cb15d12d2bfd096994c42fa800fa80231431e
Agus Setiawan <gusetiawn@gmail.com>
2021-05-28 13:18:44 +0700

    First Commit
    Create Model - Generic Pattern

---
+++ AssetManagementAPI/AssetManagementAPI/Models/RequestItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementAPI.Models
+{
+    [Table("TB_T_RequestItems")]
+    public class RequestItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int AccountId { get; set; }
+        [Required(ErrorMessage = "Tidak boleh kosong"), DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Tidak boleh kosong"), DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public string Notes { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public virtual Account Account { get; set; }
+        public virtual Item Item { get; set; }
+    }
+}
---
+++ AssetManagementAPI/AssetManagementAPI/Models/RoleAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementAPI.Models
+{
+    [Table("TB_T_RoleAccounts")]
+    public class RoleAccount
+    {
+        public int RoleId { get; set; }
+        public int AccountId { get; set; }
+        public virtual Role Role { get; set; }
+        public virtual Account Account { get; set; }
+    }
+}
