2cbade0b2db4c104ab3bbece74d54455af4d9408
marieemhmd <muhammad.marie@gmail.com>
2021-05-28 15:22:39 +0700

    Update MyContext.cs

--- AssetManagementAPI/AssetManagementAPI/Context/MyContext.cs
+++ AssetManagementAPI/AssetManagementAPI/Context/MyContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AssetManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,15 @@ namespace AssetManagementAPI.Context
         {
 
         }
-
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<RequestItem> RequestItems { get; set; }
+        public DbSet<ReturnItem> ReturnItems { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoleAccount> RoleAccounts { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
