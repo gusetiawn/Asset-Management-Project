e9fcb49d7cccc60a9a787620aa6fa97f94d39bc3
Agus Setiawan <gusetiawn@gmail.com>
2021-05-28 21:57:27 +0700

    Add UserRepository & UserController

---
+++ AssetManagementAPI/AssetManagementAPI/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using AssetManagementAPI.Base;
+using AssetManagementAPI.Models;
+using AssetManagementAPI.Repositories.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : BaseController<User, UserRepository, int>
+    {
+        public UsersController(UserRepository userRepository) : base(userRepository)
+        {
+
+        }
+    }
+}
