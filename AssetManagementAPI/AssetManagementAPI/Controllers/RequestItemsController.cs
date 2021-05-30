7af7f36ef598e457b21a39a7c56fab47cb75ff56
marieemhmd <muhammad.marie@gmail.com>
2021-05-30 19:26:07 +0700

    Add Method Request Item

--- AssetManagementAPI/AssetManagementAPI/Controllers/RequestItemsController.cs
+++ AssetManagementAPI/AssetManagementAPI/Controllers/RequestItemsController.cs
@@ -1,11 +1,14 @@
 ﻿using AssetManagementAPI.Base;
+using AssetManagementAPI.Context;
 using AssetManagementAPI.Models;
 using AssetManagementAPI.Repositories.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace AssetManagementAPI.Controllers
@@ -14,9 +17,43 @@ namespace AssetManagementAPI.Controllers
     [ApiController]
     public class RequestItemsController : BaseController<RequestItem, RequestItemRepository, int>
     {
-        public RequestItemsController(RequestItemRepository requestItemRepository) : base(requestItemRepository)
+        private readonly RequestItemRepository requestItemRepository;
+        private readonly MyContext myContext;
+        public RequestItemsController(RequestItemRepository requestItemRepository, MyContext myContext) : base(requestItemRepository)
         {
+            this.requestItemRepository = requestItemRepository;
+            this.myContext = myContext;
+        }
+        
+        [HttpPost("NewRequest")]
+        public ActionResult RequestItem(RequestItem requestItem)
+        {
+            try
+            {
+                var request = new RequestItem
+                {
+                    AccountId = requestItem.AccountId,
+                    ItemId = requestItem.ItemId,
+                    StartDate = requestItem.StartDate,
+                    EndDate = requestItem.EndDate,
+                    Quantity = requestItem.Quantity,
+                    Notes = requestItem.Notes,
+                    StatusId = 1 //Id 1 di table status aku buat "Waiting for Approval"
+                };
+                myContext.RequestItems.Add(request);
+                myContext.SaveChanges();
+
+                return StatusCode(200, new { status = HttpStatusCode.OK, message = "Request Item Berhasil"});
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(400, new { status = HttpStatusCode.BadRequest, message = "Request Item Gagal" });
+            }
 
         }
+        
+        
     }
 }
