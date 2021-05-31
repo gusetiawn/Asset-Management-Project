d7919dfca569ef404786edd81bd4d05bac839cd1
marieemhmd <muhammad.marie@gmail.com>
2021-05-31 10:54:02 +0700

    Nambah class gender

---
+++ AssetManagementAPI/AssetManagementAPI/Models/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementAPI.Models
+{
+    [Table("TB_M_Genders")]
+    public class Gender
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tidak boleh kosong")]
+        public string Name { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
