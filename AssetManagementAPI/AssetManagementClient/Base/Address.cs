0f54ec8bda701c1e9c975f83eafa88e787481823
Agus Setiawan <gusetiawn@gmail.com>
2021-06-02 10:29:46 +0700

    Add Generic Pattern for Client Project

---
+++ AssetManagementAPI/AssetManagementClient/Base/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagementApp.Base
+{
+    public class Address
+    {
+        public string link = "https://localhost:44395/API/";
+    }
+}
