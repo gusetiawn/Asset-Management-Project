173ab3e61059858d94739b2096288b9997c1d882
Agus Setiawan <gusetiawn@gmail.com>
2021-06-02 18:01:55 +0700

    Update ReturnItems, memperbaiki method

--- AssetManagementAPI/AssetManagementAPI/Controllers/ReturnItemsController.cs
+++ AssetManagementAPI/AssetManagementAPI/Controllers/ReturnItemsController.cs
@@ -5,6 +5,7 @@ using AssetManagementAPI.Models;
 using AssetManagementAPI.Repositories.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,8 +30,8 @@ namespace AssetManagementAPI.Controllers
         [HttpPost("NewRequest")] 
         public ActionResult ReturnItem(ReturnItem returnItem)
         {
-            try
-            {
+            //try
+            //{
                 var returnItm = new ReturnItem
                 {
                     RequestItemId = returnItem.RequestItemId,
@@ -40,6 +41,16 @@ namespace AssetManagementAPI.Controllers
                 myContext.ReturnItems.Add(returnItm);
                 myContext.SaveChanges();
 
+                var dataRequest = myContext.RequestItems.Where(R => R.Id == returnItem.RequestItemId).FirstOrDefault();
+                var data = myContext.Items.Include(I => I.RequestItems).Where(I => I.Id == dataRequest.ItemId).FirstOrDefault();
+                data.Quantity += dataRequest.Quantity;
+                myContext.Entry(data).State = EntityState.Modified;
+                myContext.SaveChanges();
+
+                dataRequest.StatusId = 5;
+                myContext.Entry(dataRequest).State = EntityState.Modified;
+                myContext.SaveChanges();
+
                 var reqItem = myContext.RequestItems.Where(u => u.Id == returnItem.RequestItemId).FirstOrDefault();
                 var user = myContext.Users.Where(u => u.Id == reqItem.AccountId).FirstOrDefault();
                 var subject = "Return Item Berhasil";
@@ -47,11 +58,11 @@ namespace AssetManagementAPI.Controllers
                 sendMail.SendEmail(user.Email, body, subject);
 
                 return StatusCode(200, new { status = HttpStatusCode.OK, message = "Return Item Berhasil" });
-            }
-            catch (Exception)
-            {
-                return StatusCode(400, new { status = HttpStatusCode.BadRequest, message = "Return Item Gagal" });
-            }
+            //}
+            //catch (Exception)
+            //{
+            //    return StatusCode(400, new { status = HttpStatusCode.BadRequest, message = "Return Item Gagal" });
+            //}
         }
     }
 }
