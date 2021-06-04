31b64d41f72e25a3d8433c18cf4137f8fe3a0127
Agus Setiawan <gusetiawn@gmail.com>
2021-06-04 08:27:35 +0700

    Add Admin Controller dan 3 viewnya

---
+++ AssetManagementAPI/AssetManagementClient/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementClient.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Menu/Admin/RequestItem")]
+        public IActionResult RequestItem()
+        {
+            return View();
+        }
+        [Route("Menu/Admin/Item")]
+        public IActionResult Item()
+        {
+            return View();
+        }
+        [Route("Menu/Admin/UserRegister")]
+        public IActionResult UserRegister()
+        {
+            return View();
+        }
+    }
+}
