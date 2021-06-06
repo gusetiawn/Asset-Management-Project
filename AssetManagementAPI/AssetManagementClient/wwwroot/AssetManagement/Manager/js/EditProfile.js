44986c6db3fbc8f6448ce491602c43290d7fffed
marieemhmd <muhammad.marie@gmail.com>
2021-06-06 21:23:56 +0700

    create js manager

---
+++ AssetManagementAPI/AssetManagementClient/wwwroot/AssetManagement/Manager/js/EditProfile.js
@@ -0,0 +1,143 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        url: "https://localhost:44395/API/Accounts/Profile/123452/"
+    }).done((result) => {
+        userId = `<label class="form-control-label" for="input-id">User Id</label>
+                  <input id="input-userId" class="form-control form-control-alternative" placeholder="Your Id" value="${result[0].id}" type="text" readonly>`
+        fname = `<label class="form-control-label" for="input-first-name">First name</label>
+             <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="${result[0].firstName}" readonly>`
+        lname = `<label class="form-control-label" for="input-last-name">Last name</label>
+             <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="${result[0].lastName}" readonly>`
+        birthDate = `<label class="form-control-label" for="input-birthdate">Birth Date</label>
+                 <input type="date" id="input-birthdate" class="form-control form-control-alternative" placeholder="Your Birth Date" value="${result[0].birthDate.slice(0, 10)}" readonly>`
+        gender = `<label class="form-control-label" for="input-gender">Gender</label>
+              <input type="text" id="input-gender" class="form-control form-control-alternative" placeholder="Your Gender" value="${result[0].gender}" readonly>`
+        email = `<label class="form-control-label" for="input-email">Email Address</label>
+             <input id="input-email" class="form-control form-control-alternative" placeholder="youremail@example.com" type="text" value="${result[0].email}" readonly>`
+        phone = `<label class="form-control-label" for="input-phone">Phone</label>
+             <input id="input-phone" class="form-control form-control-alternative" placeholder="+62xxx-xxxx-xxxx" type="text" value="${result[0].contact}" readonly>`
+        address = `<label class="form-control-label" for="input-address">Address</label>
+              <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="${result[0].address}" readonly>`
+        department = `<label class="form-control-label" for="input-department">Department Name</label>
+                  <input id="input-department" class="form-control form-control-alternative" placeholder="Your Department" value="${result[0].department}" type="text" readonly>`
+        $(".account-id").html(userId);
+        $(".fname").html(fname);
+        $(".lname").html(lname);
+        $(".bDate").html(birthDate);
+        $(".genderUser").html(gender);
+        $(".emailAddrs").html(email);
+        $(".contactPhone").html(phone);
+        $(".addrs").html(address);
+        $(".departmentName").html(department);
+    }).fail((error) => {
+        alert("error");
+    });
+});
+
+function EditProfile() {
+    $.ajax({
+        url: "https://localhost:44395/API/Accounts/Profile/123452/"
+    }).done((result) => {
+        fname = `<label class="form-control-label" for="input-first-name">First name</label>
+             <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="${result[0].firstName}">`
+        lname = `<label class="form-control-label" for="input-last-name">Last name</label>
+             <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="${result[0].lastName}">`
+        birthDate = `<label class="form-control-label" for="input-birthdate">Birth Date</label>
+                 <input type="date" id="input-birthdate" class="form-control form-control-alternative" placeholder="Your Birth Date" value="${result[0].birthDate.slice(0, 10)}">`
+        gender = `<label class="form-control-label" for="input-gender">Gender</label>
+                  <select name="gender" id="input-gender" class="form-control form-control-alternative">
+                    <option value="${result[0].genderId}" hidden>${result[0].gender}</option>
+                    <option value="1">Male</option>
+                    <option value="2">Female</option>
+                  </select>`
+        email = `<label class="form-control-label" for="input-email">Email Address</label>
+             <input id="input-email" class="form-control form-control-alternative" placeholder="youremail@example.com" type="text" value="${result[0].email}">`
+        phone = `<label class="form-control-label" for="input-phone">Phone</label>
+             <input id="input-phone" class="form-control form-control-alternative" placeholder="+62xxx-xxxx-xxxx" type="text" value="${result[0].contact}">`
+        address = `<label class="form-control-label" for="input-address">Address</label>
+              <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="${result[0].address}">`
+        department = `<label class="form-control-label" for="input-department">Department Name</label>
+                      <select name="department" id="input-department" class="form-control form-control-alternative">
+                        <option value="${result[0].departmentId}" hidden>${result[0].department}</option>
+                        <option value="1">Engineering</option>
+                        <option value="2">Finance</option>
+                        <option value="3">Human Capital</option>
+                      </select>`
+        buttonFinishEdit = `<a href="#"class="btn btn-primary" onclick="SubmitEdit()">Submit</a>`
+        $(".fname").html(fname);
+        $(".lname").html(lname);
+        $(".bDate").html(birthDate);
+        $(".genderUser").html(gender);
+        $(".emailAddrs").html(email);
+        $(".contactPhone").html(phone);
+        $(".addrs").html(address);
+        $(".departmentName").html(department);
+        $(".btnFinishEdit").html(buttonFinishEdit);
+    }).fail((error) => {
+        alert("error");
+    });
+}
+
+function SubmitEdit() {
+    var editProfile = new Object();
+    editProfile.Id = $('#input-userId').val();
+    editProfile.FirstName = $('#input-first-name').val();
+    editProfile.LastName = $('#input-last-name').val();
+    editProfile.GenderId = $('#input-gender').val();
+    editProfile.BirthDate = $('#input-birthdate').val();
+    editProfile.Address = $('#input-address').val();
+    editProfile.Contact = $('#input-phone').val();
+    editProfile.Email = $('#input-email').val();
+    editProfile.DepartmentId = $('#input-department').val();
+    $.ajax({
+        type: "PUT",
+        url: "https://localhost:44395/api/Users/",
+        data: JSON.stringify(editProfile),
+        contentType: "application/json; charset=utf-8",
+        datatype: "json"
+    }).done((success) => {
+        Swal.fire(
+            'Good job!',
+            'Data successfully updated !',
+            'success'
+        );
+        $.ajax({
+            url: "https://localhost:44395/API/Accounts/Profile/123452/"
+        }).done((result) => {
+            fname = `<label class="form-control-label" for="input-first-name">First name</label>
+             <input type="text" id="input-first-name" class="form-control form-control-alternative" placeholder="First name" value="${result[0].firstName}" readonly>`
+            lname = `<label class="form-control-label" for="input-last-name">Last name</label>
+             <input type="text" id="input-last-name" class="form-control form-control-alternative" placeholder="Last name" value="${result[0].lastName}" readonly>`
+            birthDate = `<label class="form-control-label" for="input-birthdate">Birth Date</label>
+                 <input type="date" id="input-birthdate" class="form-control form-control-alternative" placeholder="Your Birth Date" value="${result[0].birthDate.slice(0, 10)}" readonly>`
+            gender = `<label class="form-control-label" for="input-gender">Gender</label>
+                  <input type="text" id="input-gender" class="form-control form-control-alternative" placeholder="Your Gender" value="${result[0].gender}" readonly>`
+            email = `<label class="form-control-label" for="input-email">Email Address</label>
+             <input id="input-email" class="form-control form-control-alternative" placeholder="youremail@example.com" type="text" value="${result[0].email}" readonly>`
+            phone = `<label class="form-control-label" for="input-phone">Phone</label>
+             <input id="input-phone" class="form-control form-control-alternative" placeholder="+62xxx-xxxx-xxxx" type="text" value="${result[0].contact}" readonly>`
+            address = `<label class="form-control-label" for="input-address">Address</label>
+              <input id="input-address" class="form-control form-control-alternative" placeholder="Home Address" value="${result[0].address}" readonly>`
+            department = `<label class="form-control-label" for="input-department">Department Name</label>
+                      <input id="input-department" class="form-control form-control-alternative" placeholder="Your Department" value="${result[0].department}" type="text" readonly>`
+            buttonFinishEdit = ``
+            $(".fname").html(fname);
+            $(".lname").html(lname);
+            $(".bDate").html(birthDate);
+            $(".genderUser").html(gender);
+            $(".emailAddrs").html(email);
+            $(".contactPhone").html(phone);
+            $(".addrs").html(address);
+            $(".departmentName").html(department);
+            $(".btnFinishEdit").html(buttonFinishEdit);
+        }).fail((error) => {
+            alert("data ga keubah");
+        });
+    }).fail((notSuccess) => {
+        Swal.fire(
+            'Error!',
+            'Data failed updated !',
+            'error'
+        );
+    });
+}
---
+++ AssetManagementAPI/AssetManagementClient/wwwroot/AssetManagement/Manager/js/ListRequestItems.js
@@ -0,0 +1,164 @@
+﻿$(document).ready(function () {
+    var statusWaiting = $('#tableDataListReq').DataTable({
+        "dom": 'Bfrtip',
+        "buttons": [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
+        "ajax": {
+            "url": "https://localhost:44395/API/RequestItems/UserRequest",
+            "datatype": "json",
+            "dataSrc": ""
+        },
+        "columns": [
+            {
+                'data': null,
+                'render': function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            { 'data': 'id' },
+            { 'data': 'accountId' },
+            { 'data': 'name' },
+            { 'data': 'item' },
+            {
+                'data': 'startDate',
+                'render': function (data) {
+                    return data.slice(0, 10);
+                }
+            },
+            {
+                'data': 'endDate',
+                'render': function (data) {
+                    return data.slice(0, 10);
+                }
+            },
+            { 'data': 'quantity' },
+            { 'data': 'notes' },
+            { 'data': 'status' },
+            {
+                'data': "null",
+                'render': function (data, type, row, meta) {
+                    if (row.status == "Waiting for Approval") {
+                        return "<button type='button' class='btn' data-toggle='modal' data-target='#approveModal' onclick='findPerson(" + '"' + row.id + '"' + ")'><span style='color: lime;'><i class='far fa-check-circle'></i></span> Approve</button><button type='button' class='btn' data-toggle='modal' data-target='#rejectModal' onclick='findPerson(" + '"' + row.id + '"' + ")'><span style='color: Tomato;'><i class='far fa-times-circle'></i></span> Reject</button>";
+                    }
+                    else {
+                        return null;
+                    }
+                }
+            }
+            
+        ],
+        "columnDefs": [
+            {
+                "searchable": false,
+                "orderable": false,
+                "targets": 0
+            },
+            {
+                "targets": [1],
+                "visible": false,
+                "searchable": false
+            },
+       
+        ]
+    });
+    statusWaiting.on('order.dt search.dt', function () {
+        statusWaiting.column(0, { search: 'applied', order: 'applied' }).nodes().each(function (cell, i) {
+            cell.innerHTML = i + 1;
+        });
+    }).draw();
+});
+
+function findPerson(ReqId) {
+    var btnApprove = "";
+    btnApprove = '<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="approvedRequest(' + "'" + ReqId + "'" + ')">Yes</button><button type="button" class="btn btn-danger" data-dismiss="modal">No</button>'
+    $(".approveBtn").html(btnApprove);
+
+    var btnReject = "";
+    btnReject = '<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="rejectRequest(' + "'" + ReqId + "'" + ')">Yes</button><button type="button" class="btn btn-danger" data-dismiss="modal">No</button>'
+    $(".rejectBtn").html(btnReject);
+}
+
+function approvedRequest(IdReq) {
+    $.ajax({
+        url: "https://localhost:44395/API/RequestItems/Id=" + IdReq
+    }).done((result) => {
+        var obj = new Object();
+        obj.Id = result[0].id;
+        obj.AccountId = result[0].accountId;
+        obj.ItemId = result[0].itemId;
+        obj.StartDate = result[0].startDate;
+        obj.EndDate = result[0].endDate;
+        obj.Quantity = result[0].quantity;
+        obj.Notes = result[0].notes;
+        $.ajax({
+            type: "PUT",
+            url: "https://localhost:44395/API/RequestItems/Approve",
+            data: JSON.stringify(obj),
+            contentType: "application/json; charset=utf-8",
+            datatype: "json"
+        }).done((success) => {
+            Swal.fire(
+                'Good job!',
+                'Data successfully approved !',
+                'success'
+            );
+            $("#tableDataListReq").DataTable().ajax.reload();
+
+        }).fail((notsuccess) => {
+            Swal.fire(
+                'Error!',
+                'Data failed updated !',
+                'error'
+            );
+        });
+    }).fail((error) => {
+        Swal.fire(
+            'Error!',
+            'Data failed updated !',
+            'error'
+        );
+    });
+}
+
+function rejectRequest(IdReq) {
+    $.ajax({
+        url: "https://localhost:44395/API/RequestItems/Id=" + IdReq
+    }).done((result) => {
+        var obj = new Object();
+        obj.Id = result[0].id;
+        obj.AccountId = result[0].accountId;
+        obj.ItemId = result[0].itemId;
+        obj.StartDate = result[0].startDate;
+        obj.EndDate = result[0].endDate;
+        obj.Quantity = result[0].quantity;
+        obj.Notes = result[0].notes;
+        $.ajax({
+            type: "PUT",
+            url: "https://localhost:44395/API/RequestItems/Reject",
+            data: JSON.stringify(obj),
+            contentType: "application/json; charset=utf-8",
+            datatype: "json"
+        }).done((success) => {
+            Swal.fire(
+                'Good job!',
+                'Data successfully rejected !',
+                'success'
+            );
+            $("#tableDataListReq").DataTable().ajax.reload();
+
+        }).fail((notsuccess) => {
+            Swal.fire(
+                'Error!',
+                'Data failed updated !',
+                'error'
+            );
+        });
+    }).fail((error) => {
+        Swal.fire(
+            'Error!',
+            'Data failed updated !',
+            'error'
+        );
+    });
+}
