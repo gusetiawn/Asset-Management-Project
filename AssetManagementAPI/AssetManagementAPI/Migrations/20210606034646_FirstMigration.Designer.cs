419f60ae5a31abbed8e15fc367ca4a2c2136dab3
Agus Setiawan <gusetiawn@gmail.com>
2021-06-06 21:41:09 +0700

    Add Migration file

--- AssetManagementAPI/AssetManagementAPI/Migrations/20210606034646_FirstMigration.Designer.cs
+++ AssetManagementAPI/AssetManagementAPI/Migrations/20210606034646_FirstMigration.Designer.cs
@@ -4,14 +4,16 @@ using AssetManagementAPI.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AssetManagementAPI.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20210606034646_FirstMigration")]
+    partial class FirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
