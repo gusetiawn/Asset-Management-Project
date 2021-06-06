57d119b9d8ca6da7dd60364cbe4f1e76abe12f49
marieemhmd <muhammad.marie@gmail.com>
2021-06-06 21:19:54 +0700

    Create Controller Manager & Employee

---
+++ AssetManagementAPI/AssetManagementClient/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementClient.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Menu/RequestItem/NewRequest")]
+        public IActionResult RequestNewItem()
+        {
+            return View();
+        }
+
+        [Route("Menu/RequestItem/ListRequestItems")]
+        public IActionResult ListRequestItems()
+        {
+            return View();
+        }
+
+        [Route("Settings/Profile/Employee")]
+        public IActionResult YourAccount()
+        {
+            return View();
+        }
+
+        [Route("Settings/Security/Employee")]
+        public IActionResult Security()
+        {
+            return View();
+        }
+
+        [Route("Settings/Employee")]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
---
+++ AssetManagementAPI/AssetManagementClient/Controllers/ManagerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagementClient.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Menu/RequestList")]
+        public IActionResult RequestList()
+        {
+            return View();
+        }
+
+        [Route("Settings/Profile/Manager")]
+        public IActionResult YourAccount()
+        {
+            return View();
+        }
+
+        [Route("Settings/Security/Manager")]
+        public IActionResult Security()
+        {
+            return View();
+        }
+
+        [Route("Settings/Manager")]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
